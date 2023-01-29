b7f9167869d9f0771d0ff78e977b833e304c5095
Clément Dumas <butanium.contact@gmail.com>
2023-01-29 15:14:55 +0100

    added formatter

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,22 +2,22 @@ import sfml.graphics.*
 import sfml.window.*
 
 @main def main =
-    scala.util.Using.Manager { use =>
-        val window = use(RenderWindow(VideoMode(1024, 768), "Hello world"))
+  scala.util.Using.Manager { use =>
+    val window = use(RenderWindow(VideoMode(1024, 768), "Hello world"))
 
-        val texture = use(Texture())
-        texture.loadFromFile("src/main/resources/sfml-logo.png")
-        
-        val sprite = use(Sprite(texture))
+    val texture = use(Texture())
+    texture.loadFromFile("src/main/resources/sfml-logo.png")
 
-        while window.isOpen() do
-            for event <- window.pollEvent() do
-                event match {
-                    case _: Event.Closed => window.closeWindow()
-                    case _ => ()
-                }
+    val sprite = use(Sprite(texture))
 
-            window.clear(Color.Black())
-            window.draw(sprite)
-            window.display()
-    }
+    while window.isOpen() do
+      for event <- window.pollEvent() do
+        event match {
+          case _: Event.Closed => window.closeWindow()
+          case _               => ()
+        }
+
+      window.clear(Color.Black())
+      window.draw(sprite)
+      window.display()
+  }
