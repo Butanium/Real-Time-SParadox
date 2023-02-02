2914e9cd805c5b3cb3a645e635f50deecbcfc35f
Clément Dumas <butanium.contact@gmail.com>
2023-02-02 16:41:24 +0100

    little refactor

--- src/main/scala/engine/settings/GameSettings.scala
+++ src/main/scala/engine/settings/GameSettings.scala
@@ -3,7 +3,7 @@ package engine.settings
 import sfml.graphics.Drawable
 import sfml.internal.window.Event
 
-class Settings(val path: String = "src/main/settings/settings.json") {
+class GameSettings(val path: String = "src/main/settings/settings.json") {
   private val paramString = io.Source.fromFile(path).mkString
   private val paramJson = ujson.read(paramString)
   val keyBinding = KeyBinding(paramJson("keys"))
