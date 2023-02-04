e1d432928e46e1a378fd10eb60e0452d4b3ea751
Clément Dumas <butanium.contact@gmail.com>
2023-02-04 13:56:57 +0100

    made GameEngine actually useful

--- src/main/scala/engine/GameEngine.scala
+++ src/main/scala/engine/GameEngine.scala
@@ -1,8 +1,19 @@
 package engine
 
-class GameEngine(window : sfml.window.Window) {
-  val gameInfo: GameInfo = new GameInfo
-  def step(delta: Float) =
-    ()
+import scala.collection.mutable.ListBuffer
+import engine.objects.GameObject
+import sfml.graphics.RenderWindow
 
+class GameEngine() {
+  val gameInfo: GameInfo = new GameInfo
+  val gameObjects: ListBuffer[GameObject] = ListBuffer.empty[GameObject]
+  def step(delta: Float) =
+    gameObjects.foreach(_.update())
+    gameObjects.filter(x =>
+      if !x.active then { x.delete(); false }
+      else true
+    )
+
+  def render(window: RenderWindow) =
+    gameObjects.foreach(window.draw(_))
 }
