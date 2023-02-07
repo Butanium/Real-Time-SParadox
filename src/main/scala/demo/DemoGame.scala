c49f6528011bdaf8b03044e314b758d2b6cde544
Clément Dumas <butanium.contact@gmail.com>
2023-02-07 17:34:51 +0100

    modified game so that we let sfml handle the frameRate

--- src/main/scala/demo/DemoGame.scala
+++ src/main/scala/demo/DemoGame.scala
@@ -5,11 +5,13 @@ import engine2D.graphics.Group
 import engine2D.GameEngine
 import engine2D.objects.GraphicObject
 import engine2D.Game
-class DemoGame(debug: Boolean = true)
-    extends Game(60, sfml.graphics.Color(0, 0, 100), debug = debug) {
+import sfml.graphics.RenderWindow
+class DemoGame(window: RenderWindow, debug: Boolean = true)
+    extends Game(window, 60, sfml.graphics.Color(0, 0, 100), debug = debug) {
   val engine = GameEngine(1, debug = debug)
-  def init() = {
+  override def init() = {
     val ourObject = DemoObject(engine = engine)
     engine.spawn(ourObject)
+    super.init()
   }
 }
--- src/main/scala/demo/StaticGame.scala
+++ src/main/scala/demo/StaticGame.scala
@@ -5,13 +5,15 @@ import sfml.graphics.Texture
 import sfml.graphics.Sprite
 import engine2D.objects.GraphicObject
 
-class StaticGame extends engine2D.Game {
+class StaticGame(window: sfml.graphics.RenderWindow)
+    extends engine2D.Game(window) {
   val engine = GameEngine(1)
-  def init(): Unit = {
+  override def init(): Unit = {
     val sfml_img = Texture()
     sfml_img.loadFromFile("src/main/resources/sfml-logo.png")
     val sprite = Sprite(sfml_img)
     sprite.position = (400, 400)
     engine.spawn(GraphicObject(sprite, engine))
+    super.init()
   }
 }
