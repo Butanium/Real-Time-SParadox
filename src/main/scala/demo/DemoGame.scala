1037aaf5607018fe2b470c447a573c2469e6b03e
Clément Dumas <butanium.contact@gmail.com>
2023-02-07 16:25:15 +0100

    removed useless .apply

--- src/main/scala/demo/DemoGame.scala
+++ src/main/scala/demo/DemoGame.scala
@@ -6,7 +6,7 @@ import engine2D.GameEngine
 import engine2D.objects.GraphicObject
 import engine2D.Game
 class DemoGame(debug: Boolean = true)
-    extends Game(60, sfml.graphics.Color.apply(0, 0, 100), debug = debug) {
+    extends Game(60, sfml.graphics.Color(0, 0, 100), debug = debug) {
   val engine = GameEngine(1, debug = debug)
   def init() = {
     val ourObject = DemoObject(engine = engine)
