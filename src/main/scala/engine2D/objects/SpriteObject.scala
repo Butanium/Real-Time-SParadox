de85f64818ae06b58a736217b7e453515a5c1a1f
Clément Dumas <butanium.contact@gmail.com>
2023-02-08 23:40:47 +0100

    small refactor and added trait boundable to spriteObject

--- src/main/scala/engine2D/objects/SpriteObject.scala
+++ src/main/scala/engine2D/objects/SpriteObject.scala
@@ -1,6 +1,8 @@
 package engine2D.objects
 
 import engine2D.GameEngine
+import sfml.graphics.Rect
+import sfml.graphics.Sprite
 
 /** A SpriteObject is a GraphicObject that is instantiated with a Texture.
   * @param texture
@@ -15,4 +17,42 @@ class SpriteObject(
     texture: sfml.graphics.Texture,
     engine: GameEngine,
     active: Boolean = true
-) extends GraphicObject(sfml.graphics.Sprite(texture), engine, active) {}
+) extends GraphicObject(sfml.graphics.Sprite(texture), engine, active)
+    with Boundable {
+
+  /** The Sprite of this SpriteObject.
+    */
+  val sprite: Sprite = this.drawable.asInstanceOf[Sprite]
+
+  /** The color of the sprite.
+    */
+  def color: sfml.graphics.Color = sprite.color
+
+  /** Set the color of the sprite.
+    * @param c
+    *   The new color of the sprite.
+    */
+  def color_=(c: sfml.graphics.Color): Unit = sprite.color = c
+
+  /** Get the local bounds of the sprite.
+    * @return
+    *   The local bounds of the sprite.
+    * @note
+    *   The local bounds are the bounds of the sprite before it is transformed.
+    */
+  def localBounds: Rect[Float] =
+    sprite.localBounds
+
+  /** Get the global bounds of the sprite.
+    * @return
+    *   The global bounds of the sprite.
+    * @note
+    *   The global bounds are the bounds of the sprite after it is transformed.
+    *   It is therefore relative to the global transform of the sprite.
+    */
+  def globalBounds: Rect[Float] =
+    globalTransform.transformRect(localBounds)
+
+  def bounds = globalBounds
+
+}
