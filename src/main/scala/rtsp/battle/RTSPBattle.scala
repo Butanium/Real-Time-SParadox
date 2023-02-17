d0537facbc7352ae59402f78da39a18e8e81718e
Azammat CZ <azammat.cz@gmail.com>
2023-02-17 17:53:45 +0100

    begin warrior attack

--- src/main/scala/rtsp/battle/RTSPBattle.scala
+++ src/main/scala/rtsp/battle/RTSPBattle.scala
@@ -6,38 +6,11 @@ import scala.collection.mutable.ListBuffer
     Il faut stocker les équipes dans une liste de liste de warriors: List[List[RTSPWarriors]]
 */
 
-class RTSPBattle {
-  val teams = ListBuffer[List[RTSPWarrior]]()
+class RTSPBattle(private val team1 : List[RTSPWarrior], private val team2 : List[RTSPWarrior]) {
   // Lancer la bataille: faire bouger les warriors non morts
+    private val teams = Array[List[RTSPWarrior]](team1, team2)
     def step() = {
-        /*
-        // Boucle de tour
-        // Check si une des teams est totalement morte
-        val losers = ListBuffer[Int]()
-        while (losers.length == 0) {
-            // Boucle de tour
-            for (team <- teams) {
-                for (warrior <- team) {
-                    if (warrior.health > 0) {
-                        // Warrior doit bouger
-                        // Warrior doit attaquer
-                    }
-                }
-            }
-            // Check si une des teams est totalement morte
-            for (team <- teams) {
-                var dead = true
-
-                for (warrior <- team) {
-                    if (warrior.health > 0) {
-                        dead = false
-                    }
-                }
-                if (dead) {
-                    losers += teams.indexOf(team)
-                }
-            }
-        }*/
+        // On effectue une étape de combat, et on renvoie la liste de perdants à chaque étape (dès qu'elle n'est plus vide, le combat est terminé)
         val losers = ListBuffer[Int]()
         for (team <- teams) {
             var dead = true
@@ -54,10 +27,20 @@ class RTSPBattle {
         for (team <- teams) {
             for (warrior <- team) {
                 if (warrior.health > 0) {
-                    // Warrior doit bouger
-                    // Warrior doit attaquer
+                    // Le warrior agit
+                    warrior.behavior.evaluate(warrior)
                 }
             }
         }
+        losers
     }
+    // fonction alliés / ennemis
+    def getEnnemies(idTeam : Int) = {
+        teams(1-idTeam)
+    }
+
+    def getAllies(idTeam : Int) = {
+        teams(idTeam)
+    }
+    
 }
--- src/main/scala/rtsp/objects/RTSPWarrior.scala
+++ src/main/scala/rtsp/objects/RTSPWarrior.scala
@@ -2,13 +2,17 @@ package rtsp.objects
 import engine2D.objects.GameUnit
 import rtsp.RTSPGameEngine
 import rtsp.battle.RTSPBattle
+import rtsp.battle.Behavior
 
 /*
   Utiliser l'arbre de comportement
   Chaque warrior a une équipe: 0 = joueur, 1 = ennemi
 */
 
-class RTSPWarrior (engine: RTSPGameEngine, battle: RTSPBattle, var team: Int, var range : Int, var damage : Int)
+class RTSPWarrior (engine: RTSPGameEngine, battle: RTSPBattle, var team: Int, var range : Int, var damage : Int, var behavior : Behavior)
   extends GameUnit(100, 1f, engine, baseRotation = 0, active = true) {
-
+    var target : Option[RTSPWarrior] = None
+    def attack() : Unit = {
+      
+    }
 }
