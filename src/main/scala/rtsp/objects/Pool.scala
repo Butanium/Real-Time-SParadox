a4ac0978e1230862e8fa90913a2ae8cab93f2142
PaulB06 <paulbigot06@gmail.com>
2023-02-18 13:51:59 +0100

    fonctions du pool

--- src/main/scala/rtsp/objects/Pool.scala
+++ src/main/scala/rtsp/objects/Pool.scala
@@ -1,6 +1,17 @@
 package rtsp.objects
-
-abstract class Pool {
-    def get_random : ShopWarrior
-    //A définir
+import util.Random
+import rtsp.objects
+abstract class Pool(type_prob_warrior: Array [Tuple2[ShopWarrior,  Float]]) {
+    def coeff_sum (t:Array [Tuple2[ShopWarrior,  Float]]):Float =
+        var s:Float = 0
+        for i <- 0 to ((t.size) -1) do
+            s += (t(i))._2
+        s
+    def get_random: ShopWarrior =
+        var p:Float = Random.between(0f,coeff_sum(type_prob_warrior))
+        var i: Int = 0
+        while (p >= 0f) && (i < type_prob_warrior.size) do
+            p -= type_prob_warrior(i)._2
+            i += 1
+        type_prob_warrior(i-1)._1
 }
