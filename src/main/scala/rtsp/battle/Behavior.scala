d9009b357c687f92ae1958c6d15d55707f125455
Clément Dumas <butanium.contact@gmail.com>
2023-02-18 01:14:20 +0100

    finished early version of the behavior tree

--- src/main/scala/rtsp/battle/Behavior.scala
+++ src/main/scala/rtsp/battle/Behavior.scala
@@ -1,42 +1,123 @@
 package rtsp.battle
 import rtsp.objects.RTSPWarrior
+import scala.util.control.NonLocalReturns
 
-enum TargetType {
-  case Enemy
-  case Ally
+enum Action(val targetType: TargetType) {
+  case Attack(_targetType: TargetType) extends Action(_targetType)
+  case Move(_targetType: TargetType) extends Action(_targetType)
+}
+
+enum TargetType(val targetFilter: TargetFilter) {
+  case Enemy(_targetFilter: TargetFilter) extends TargetType(_targetFilter)
+  case Ally(_targetFilter: TargetFilter) extends TargetType(_targetFilter)
 }
 
 enum TargetFilter {
-  case Closest(targetType : TargetType)
+  case Closest
 }
-
-enum Action(val targetFilter : TargetFilter) {
-  case Attack(_targetFilter: TargetFilter) extends Action(_targetFilter)
-  case Move(_targetFilter: TargetFilter) extends Action(_targetFilter)
-}
-
 // enum Condition {
 //   case Health()
 // }
 
 enum BehaviorTree {
   case ActionNode(action: Action)
-  case Node(children : List[BehaviorTree])
+  case Node(children: List[BehaviorTree])
   // case ConditionNode()
 }
-class Behavior(tree : BehaviorTree) {
-  def evaluate(warrior : RTSPWarrior) : Unit = {
+class Behavior(val tree: BehaviorTree, val battle: RTSPBattle) {
+
+  def evaluate(warrior: RTSPWarrior) = {
+    warrior.target = None
+    warrior.rooted = true
+    evaluateNode(warrior, tree)
+  }
+  def evaluateNode(warrior: RTSPWarrior, tree: BehaviorTree): Boolean = {
+    import BehaviorTree._
+    tree match
+      case ActionNode(action) => return evaluateAction(warrior, action)
+      case Node(children) =>
+        children.exists(evaluateNode(warrior, _))
+    false
+  }
+
+  /** Evaluate if an action is valid. If it is, the action is executed.
+    * @param warrior
+    *   The warrior that will execute the action
+    * @param action
+    *   The action to evaluate
+    * @return
+    *   true if the action was executed, false otherwise
+    */
+  def evaluateAction(warrior: RTSPWarrior, action: Action): Boolean = {
+    action match
+      case Action.Attack(targetType) => {
+        val potentialTargets =
+          evaluateTargetType(warrior, targetType).filter(warrior.canAttack(_))
+        evaluateTargetFilter(
+          warrior,
+          targetType.targetFilter,
+          potentialTargets
+        ) match {
+          case None => false
+          case Some(target) =>
+            warrior.target = Some(target)
+            true
+        }
+      }
+      case Action.Move(targetType) =>
+        evaluateTargetFilter(
+          warrior,
+          targetType.targetFilter,
+          evaluateTargetType(warrior, targetType)
+        ) match {
+          case None => false
+          case Some(target) =>
+            warrior.rooted = false
+            warrior.changeDirectionTo(target)
+            true
+        }
 
   }
 
-}
+  /** Evaluate the target type and returns the possible targets
+    * @param warrior
+    *   The warrior that will execute the action
+    * @param targetType
+    *   The target type to evaluate
+    */
+  def evaluateTargetType(
+      warrior: RTSPWarrior,
+      targetType: TargetType
+  ): List[RTSPWarrior] = {
+    targetType match
+      case TargetType.Enemy(filter) => battle.getAllies(warrior.team)
+      case TargetType.Ally(filter)  => battle.getEnemies(warrior.team)
+  }
 
+  /** Evaluate the target filter and return the target
+    * @param warrior
+    *   The warrior that will execute the action
+    * @param targetFilter
+    *   The target filter to evaluate
+    */
+  def evaluateTargetFilter(
+      warrior: RTSPWarrior,
+      targetFilter: TargetFilter,
+      potentialTargets: List[RTSPWarrior]
+  ): Option[RTSPWarrior] = {
+    targetFilter match
+      case TargetFilter.Closest =>
+        if (potentialTargets.nonEmpty)
+          Some(potentialTargets.minBy(warrior.distanceTo(_)))
+        else None
+  }
+}
 
 object Behavior {
   import Action._
   import TargetFilter._
   import TargetType._
   import BehaviorTree._
-  def basicBehavior = 
-    Node(List(ActionNode(Attack(Closest(Enemy)))))
+  def basicBehavior =
+    Node(List(ActionNode(Attack(Enemy(Closest)))))
 }
--- src/main/scala/rtsp/objects/RTSPWarrior.scala
+++ src/main/scala/rtsp/objects/RTSPWarrior.scala
@@ -7,16 +7,33 @@ import rtsp.battle.Behavior
 /*
   Utiliser l'arbre de comportement
   Chaque warrior a une équipe: 0 = joueur, 1 = ennemi
-*/
+ */
 
-class RTSPWarrior (engine: RTSPGameEngine, battle: RTSPBattle, var team: Int, var range : Int, var attackDamage : Int, var behavior : Behavior, var attackDelay : Float)
-  extends GameUnit(100, 1f, engine, baseRotation = 0, active = true) {
-    var target : Option[RTSPWarrior] = None
-    var currentAttackDelay = attackDelay
-    def attack() : Unit =
-      target match
-        case None => currentAttackDelay = attackDelay
-        case Some(warrior : RTSPWarrior) => if (currentAttackDelay < 0) then {warrior.health -= attackDamage} else {currentAttackDelay -= engine.deltaTime}
-      
-    
+class RTSPWarrior(
+    engine: RTSPGameEngine,
+    battle: RTSPBattle,
+    var team: Int,
+    var range: Int,
+    var attackDamage: Int,
+    var behavior: Behavior,
+    var attackDelay: Float
+) extends GameUnit(100, 1f, engine, baseRotation = 0, active = true) {
+  var target: Option[RTSPWarrior] = None
+  var currentAttackDelay = attackDelay
+  def attack(): Unit =
+    target match
+      case None => currentAttackDelay = attackDelay
+      case Some(warrior: RTSPWarrior) =>
+        if (currentAttackDelay < 0) then { warrior.health -= attackDamage }
+        else { currentAttackDelay -= engine.deltaTime }
+  def canAttack(target: RTSPWarrior): Boolean = {
+    distanceTo(target) <= range
+  }
+  def canMove(target: engine2D.objects.GameTransform) = true
+
+  override def update(): Unit = {
+    super.update()
+    attack()
+  }
+
 }
