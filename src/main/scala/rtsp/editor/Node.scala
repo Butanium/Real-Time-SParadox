79dfa9e564e98e41b5bfff1f498d09e1ed8d1013
PaulB06 <paulbigot06@gmail.com>
2023-05-14 18:22:34 +0200

    deletion finished

--- src/main/scala/rtsp/editor/Node.scala
+++ src/main/scala/rtsp/editor/Node.scala
@@ -7,38 +7,92 @@ import sfml.window.Mouse
 import engine2D.objects.RectangleObject
 import rtsp.Constants
 import engine2D.objects.CircleObject
-import engine2D.objects.LineObject
+import rtsp.editor.LineObject
 import engine2D.eventHandling.MouseEvent
 import sfml.graphics.Color
 import scala.collection.mutable.ListBuffer
+import sfml.system.Vector2
 
 class Node(engine: GameEngine)
     extends RectangleObject(Constants.NODE_WIDTH, Constants.NODE_HEIGHT, engine)
     with Grabbable(Mouse.Button.Left, engine) {
-  val square = RectangleObject(Constants.NODE_CIRCLE_RADIUS, Constants.NODE_CIRCLE_RADIUS, engine)
+  Node.nodeList += this
+  val square = RectangleObject(
+    Constants.NODE_CIRCLE_RADIUS,
+    Constants.NODE_CIRCLE_RADIUS,
+    engine
+  )
   square.zIndex = 10
   square.fillColor = Color.Red()
   addChildren(square)
-  square.position = (Constants.NODE_WIDTH / 2f - Constants.NODE_CIRCLE_RADIUS/2f, Constants.NODE_HEIGHT)
+  square.position = (
+    Constants.NODE_WIDTH / 2f - Constants.NODE_CIRCLE_RADIUS / 2f,
+    Constants.NODE_HEIGHT
+  )
   val follower = new GameObject(engine)
-  val childrenNode : ListBuffer [Node] = ListBuffer.empty
-  def release(line :LineObject): Unit =
-    val childNode = new Node(engine)
+  val childrenNode: ListBuffer[Node] = ListBuffer.empty
+  val parentsNode: ListBuffer[Node] = ListBuffer.empty
+  val linesLinked: ListBuffer[LineObject] = ListBuffer.empty
+  def release(line: LineObject): Unit =
+    val childNode =
+      Node.searchNode(engine.mouseManager.mouseState.worldPos) match
+        case Some(node) => node
+        case None =>
+          val n = Node(engine)
+          n.position = engine.mouseManager.mouseState.worldPos
+          n
     childrenNode.addOne(childNode)
-    childNode.position = engine.mouseManager.mouseState.worldPos
+    childNode.parentsNode.addOne(this)
     engine.spawn(childNode)
     line.target2 = childNode
     line.addPos2 = (Constants.NODE_WIDTH / 2f, 0f)
+    childNode.linesLinked += line
   def whenClickedCircle() =
-    val line = LineObject(Constants.LINE_THICKNESS, this, follower, (Constants.NODE_WIDTH / 2f, Constants.NODE_HEIGHT + Constants.NODE_CIRCLE_RADIUS / 2f), (0f,0f), engine)
+    val line = LineObject(
+      Constants.LINE_THICKNESS,
+      this,
+      follower,
+      (
+        Constants.NODE_WIDTH / 2f,
+        Constants.NODE_HEIGHT + Constants.NODE_CIRCLE_RADIUS / 2f
+      ),
+      (0f, 0f),
+      engine
+    )
+    linesLinked += line
     engine.spawn(line)
-    listenToMouseEvent(MouseEvent.ButtonReleased(Mouse.Button.Left, false), () => release(line))
+    listenToMouseEvent(
+      MouseEvent.ButtonReleased(Mouse.Button.Left, false),
+      () => release(line)
+    )
   listenToMouseEvent(
     MouseEvent.BoundsPressed(square, Mouse.Button.Left, true),
     whenClickedCircle
   )
+  def delete() =
+    parentsNode.foreach((node: Node) => node.childrenNode -= this)
+    childrenNode.foreach((node: Node) => node.parentsNode -= this)
+    linesLinked.foreach((line: LineObject) => line.deleteWithout(this))
+    this.markForDeletion()
+
+  listenToBoundsClicked(Mouse.Button.Right, this, false, delete)
+
+  
   override protected def onUpdate(): Unit =
     follower.position = engine.mouseManager.mouseState.worldPos
     super.onUpdate()
+
+  override protected def onDeletion(): Unit =
+    Node.nodeList -= this
+    super.onDeletion()
+
+}
+
+object Node {
+  val nodeList: ListBuffer[Node] = ListBuffer.empty
+  def isInNode(point: Vector2[Float], node: Node) =
+    node.contains(point)
+  def searchNode(point: Vector2[Float]) =
+    nodeList.find((node: Node) => isInNode(point, node))
 
 }
