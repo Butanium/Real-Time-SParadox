4f7fc77ba959124b215d0067894d85ee0009f8d3
PaulB06 <paulbigot06@gmail.com>
2023-05-14 18:25:39 +0200

    deldete this

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,8 +3,10 @@ import sfml.window.*
 import engine2D.graphics.Group
 import sfml.system.Vector2
 import rtsp.RTSPShopGame
+import rtsp.NodeGame
 
 @main def main =
   val window = RenderWindow(VideoMode(800, 800), "RTSP")
-  val demo = RTSPShopGame(window)
+  val demo = NodeGame(window)
+  // val demo = RTSPShopGame(window)
   demo.gameLoop()
